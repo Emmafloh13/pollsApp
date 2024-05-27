3d56a1e16be388cf77430118eac80e1fd7209db2
Emmafloh13 <emmafh342@gmail.com>
2024-05-27 13:52:00 -0600

    3re comit implementando cantidad de votos

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question
+
 
 # Create your views here.
 
@@ -11,9 +12,23 @@ def home (request):
     )
 
 def vote (request, q_id):
-    q = Question.objects.get(pk=q_id)
-    return render(request, 'poll/vote.html',{
+    q = get_object_or_404(Question, pk=q_id)
+    if request.method == "POST":
+        choice_id = request.POST.get('choice')
+        choice = q.choice_set.get(pk=choice_id)
+        choice.vote += 1
+        choice.save()
+        return redirect('poll:result',  q_id) 
+    return render(request, 'poll/vote.html', {
+            "question": q
+    })
+
+def result (request, q_id):
+    try:
+        q = Question.objects.get(pk=q_id)
+    except Question.DoesNotExist:
+        return redirect('poll:home')
+    return render(request, 'poll/result.html', {
         "question": q
     })
-def result (request, id):
-    pass
+ 
