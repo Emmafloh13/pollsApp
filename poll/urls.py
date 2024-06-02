2b661e82e63d4be30fa5544bd803b8e7ca2ee17a
Emmafloh13 <emmafh342@gmail.com>
2024-06-01 20:06:25 -0600

    4to comit con grafica de votos y link para ir a las respuestas

--- poll/urls.py
+++ poll/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 
     path('<int:q_id>/vote/', views.vote, name = 'vote'),
 
-    path('<int:q_id>/result/', views.result, name = 'result')
+    path('<int:q_id>/result/', views.result, name = 'result'),
+
+    path('api/vote_data/<int:q_id>/', views.get_vote_data, name='get_vote_data'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Question
+from .models import Question, Choice
+from django.http import JsonResponse
 
 
 # Create your views here.
+
+def get_vote_data(request, q_id):
+    question = get_object_or_404(Question, pk=q_id)
+    choices = [{'choice_text': choice.choice_text, 'vote_count': choice.vote} for choice in question.choice_set.all()]
+    data = ({'labels': [choice['choice_text'] for choice in choices], 'data': [choice['vote_count'] for choice in choices]})
+    return JsonResponse(data)
 
 def home (request):
     questions = Question.objects.all()
@@ -14,14 +21,20 @@ def home (request):
 def vote (request, q_id):
     q = get_object_or_404(Question, pk=q_id)
     if request.method == "POST":
-        choice_id = request.POST.get('choice')
-        choice = q.choice_set.get(pk=choice_id)
-        choice.vote += 1
-        choice.save()
-        return redirect('poll:result',  q_id) 
-    return render(request, 'poll/vote.html', {
-            "question": q
+        try:
+            choice_id = request.POST.get('choice')
+            choice = q.choice_set.get(pk=choice_id)
+            choice.vote += 1
+            choice.save()
+            return redirect('poll:result',  q_id=q_id) 
+        except (KeyError, Choice.DoesNotExist):
+            return render (request, 'poll/vote.html', {
+                "question": q,
+                "error_message": "Para votar debes una opción"
     })
+    return render(request, 'poll/vote.html', {
+    "question": q
+})
 
 def result (request, q_id):
     try:
